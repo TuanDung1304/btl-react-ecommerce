0201d00d21d8b8e4c66151122daf4b1321b246c0
TuanDung1304 <tuandung13401@gmail.com>
2023-10-12 17:45:25 +0700

    Best seller slider

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { Box, Button, Grid, Typography } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
 import CategoryGridItem from './CategoryGridItem'
+import CollectionGridItem from './CollectionGridItem'
+import SimpleSlider from './SimpleSlider'
 
 const useStyles = makeStyles()(() => ({
   root: {
@@ -75,6 +77,31 @@ const useStyles = makeStyles()(() => ({
       },
     },
   },
+<<<<<<< Updated upstream
+=======
+  outstanding: {
+    marginTop: 20,
+    width: '90%',
+
+    '& div': {
+      width: '100%',
+      height: '300px',
+      overflow: 'hidden',
+    },
+
+    '& img': {
+      width: '100%',
+      maxHeight: '100%',
+      objectFit: 'cover',
+    },
+
+    '@media (min-width: 900px)': {
+      flexDirection: 'row',
+      width: 'auto',
+      '& div': { height: 'auto' },
+    },
+  },
+>>>>>>> Stashed changes
 }))
 
 export default function Home() {
@@ -112,12 +139,36 @@ export default function Home() {
         />
       </Grid>
 
+      <SimpleSlider />
+
       <Box className={classes.trandingNavigate}>
         <Typography>Get the Look</Typography>
         <Typography fontWeight={26}>FASHION TRENDS</Typography>
         <Typography>Fashion Trends Note</Typography>
         <Button variant="outlined">SHOP NOW</Button>
       </Box>
+<<<<<<< Updated upstream
+=======
+
+      <Grid
+        container
+        className={classes.outstanding}
+        columnSpacing={{ md: 2 }}
+        rowSpacing={2}>
+        <CollectionGridItem
+          img="src/assets/newCollection.jpg"
+          content="STREETSTYLE"
+          title="NEW COLLECTION"
+          url="products/street-style"
+        />
+        <CollectionGridItem
+          img="src/assets/summerSale.jpg"
+          content="UP TO 60%"
+          title="SUMMER SALE"
+          url="products/summer-sale"
+        />
+      </Grid>
+>>>>>>> Stashed changes
     </Box>
   )
 }
